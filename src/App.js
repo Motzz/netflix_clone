67ab4a32217514dfc0e4566a8ea5b40d4616adc0
Motzz <s160419007@student.ubaya.ac.id>
2023-03-05 21:26:13 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
 
+import Nav from "./navbar/Nav";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <div className="container">
+        <Nav />
+
+        <div className="kartu-film1">
+          <img src="https://assets.nflxext.com/ffe/siteui/vlv3/a43711df-c428-4f88-8bb3-b2ac5f20608f/d018d3cd-2602-44a4-a9e2-5b66e5fef473/ID-id-20230227-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
+          <div className="text-wrapper">
+            <h1 className="text-h1">Film, acara TV tak terbatas, dan lebih banyak lagi.</h1>
+            <h2 className="text-h2">Tonton dimana pun dan diperangkat apa pun, mulai dari Rp65.000/bulan.</h2>
+            <p className="text-h3">Siap menonton? Masukkan email untuk membuat atau memulai lagi keanggotaanmu</p>
+            <div className="email-wrapper">
+              <input type="text" value="Alamat email" className="text"></input>
+              <button className="button">Mulai > </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="kartu-film2">
+          <div className="text-film2">
+            <h1>Nikmati di TV-mu.</h1>
+            <h2>Tonton di Smart TV, Playstation, Xbox, Chromecast, Apple TV, pemutar Blu-ray, dan banyak lagi.</h2>
+          </div>
+          <div className="gambar-text2">
+            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" />
+          </div>
+        </div>
+        <div className="kartu-film3">
+          <div className="text-film3">
+            <h1>Download acara TV untuk menontonnya secara offline.</h1>
+            <h2>Simpan favoritmu dengan mudah agar selalu ada acara TV dan film yang bisa ditonton.</h2>
+          </div>
+          <div className="gambar-text3">
+            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" />
+          </div>
+        </div>
+        <div className="kartu-film4">
+          <div className="text-film4">
+            <h1>Tonton di mana pun.</h1>
+            <h2>Streaming film dan acara TV tak terbatas di ponsel, tablet, laptop, dan TV-mu.</h2>
+          </div>
+          <div className="gambar-text4">
+            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-id.png" />
+          </div>
+        </div>
+        <div className="kartu-film5">
+          <div className="text-film5">
+            <h1>Buat profil untuk anak.</h1>
+            <h2>Kirim anak-anak untuk bertualang bersama karakter favorit mereka di dunia yang dibuat khusus untuk mereka—gratis dengan keanggotaanmu.</h2>
+          </div>
+          <div className="gambar-text5">
+            <img src="https://occ-0-4566-993.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABQzEb4vjsdT9D7Ptz3RFZUCJhQWl7hhFgeydEmKjJJbrodhfBkjir2wvq3XksYKVPj-TfOZ9rz19ZPPWaUjPA4IfvIwJEyfXVgv_.png?r=fee" />
+          </div>
+        </div>
+
+        <div className="kartu-film6">
+          <div className="text-film6">
+            <h1>Tanya Jawab Umum</h1>
+          </div>
+          <div className="kotak-text6">
+            <h1>Apa itu netflix </h1>
+            <div className="logo">
+              <svg id="thin-x" width="20" height="20" fill="white" viewBox="0 0 26 26" class="svg-icon svg-icon-thin-x svg-closed" focusable="true">
+                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+              </svg>
+            </div>
+          </div>
+
+          <div className="kotak-text6">
+            <h1>Apa itu netflix </h1>
+            <div className="logo">
+              <svg id="thin-x" width="20" height="20" fill="white" viewBox="0 0 26 26" class="svg-icon svg-icon-thin-x svg-closed" focusable="true">
+                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+              </svg>
+            </div>
+          </div>
+
+          <div className="kotak-text6">
+            <h1>Apa itu netflix </h1>
+            <div className="logo">
+              <svg id="thin-x" width="20" height="20" fill="white" viewBox="0 0 26 26" class="svg-icon svg-icon-thin-x svg-closed" focusable="true">
+                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+              </svg>
+            </div>
+          </div>
+
+          <div className="kotak-text6">
+            <h1>Apa itu netflix </h1>
+            <div className="logo">
+              <svg id="thin-x" width="20" height="20" fill="white" viewBox="0 0 26 26" class="svg-icon svg-icon-thin-x svg-closed" focusable="true">
+                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="kartu-film7">
+          <div className="text-film7">
+            <h1>Buat profil untuk anak.</h1>
+            <h2>Kirim anak-anak untuk bertualang bersama karakter favorit mereka di dunia yang dibuat khusus untuk mereka—gratis dengan keanggotaanmu.</h2>
+          </div>
+          <div className="gambar-text7">
+            <img src="https://occ-0-4566-993.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABQzEb4vjsdT9D7Ptz3RFZUCJhQWl7hhFgeydEmKjJJbrodhfBkjir2wvq3XksYKVPj-TfOZ9rz19ZPPWaUjPA4IfvIwJEyfXVgv_.png?r=fee" />
+          </div>
+        </div>
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +120,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      </div>
     </div>
   );
 }
